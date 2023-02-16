60ce04fe121ccc6220ce3e3f35d455199009a96d
Enes Önal <enes.onal777@gmail.com>
2023-02-16 21:14:39 +0300

    default swift codes

---
+++ Section 2/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Enes Önal on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
