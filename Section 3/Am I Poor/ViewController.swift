da238e5047e599e13d298f56deb5691ad9440d38
Enes Önal <enes.onal777@gmail.com>
2023-02-16 21:40:55 +0300

    default swift folders

---
+++ Section 3/Am I Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Am I Poor
+//
+//  Created by Enes Önal on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
